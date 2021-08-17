926671c3b0175876ae6f5592b7e573484dc40ac7
rezaie99 <ghannad00@yahoo.com>
2021-08-17 17:20:33 -0400

    okay

--- main.jl
+++ main.jl
@@ -9,3 +9,26 @@ function foo(data, origin)
 end
 p = foo(cars, "USA")
 p |> save("foo.png")
+
+using DataFramesMeta, RCall, DataFrames
+using Statistics
+flights = rcopy(R"nycflights13::flights")
+@linq flights |>
+    where(:month .== 1, :day .< 5) |>
+    orderby(:day, :distance) |>
+    select(:month, :day, :distance, :air_time)  |>
+    transform(speed = :distance ./ :air_time * 60) |>
+    by(:day, avgspeed = mean(skipmissing(:speed)))
+
+using StatsPlots
+(@linq flights |> where((!ismissing).(:air_time))) |> @df histogram(:air_time)
+
+flights[1:10^4, :] |> @df scatter(:air_time, :distance, group=:carrier)
+
+using Optim
+rosenbrock(x) = (1.0 - x[1])^2 + 100.0 * (x[2] - x[1]^2)^2
+result = optimize(rosenbrock, zeros(2), BFGS())
+
+using Roots
+f(x) = exp(x) - x^4
+find_zero(f, 3)
